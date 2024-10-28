6792111847cb4ce78e1c9ee88f2accf9ea25a1e1
DoomDuck <docdoomduck@protonmail.com>
2024-10-28 14:04:27 +0100

    fix: Quote variables in start.sh

--- tools/start.sh
+++ tools/start.sh
@@ -5,12 +5,12 @@ echo "Starting installation"
 
 # Reference: https://www.mediawiki.org/wiki/Manual:Install.php
 # TODO: Use $DATABASE_NAME
-php maintenance/run.php install "42 Community" $ADMIN_USERNAME \
-    --pass=$ADMIN_PASSWORD \
-    --server=$SERVER_URL \
-    --dbserver=$DATABASE_URL \
-    --dbuser=$DATABASE_USER \
-    --dbpass=$DATABASE_PASSWORD \
+php maintenance/run.php install "42 Community" "$ADMIN_USERNAME" \
+    --pass="$ADMIN_PASSWORD" \
+    --server="$SERVER_URL" \
+    --dbserver="$DATABASE_URL" \
+    --dbuser="$DATABASE_USER" \
+    --dbpass="$DATABASE_PASSWORD" \
     --with-extensions
 
 
